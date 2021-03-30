35cd5ec742661cdd74fed4975bbff4b957fe54a7
Ubuntu <ubuntu@ip-172-31-7-238.ap-northeast-1.compute.internal>
2021-03-30 08:26:48 +0000

    fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,9 +16,8 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find(params[:id])
   end
-end
 
-def update
+  def update
   @task = Task.find(params[:id])
 
   if @task.update(task_params)
@@ -28,12 +27,29 @@ def update
     flash.now[:danger] = 'タスクが編集されませんでした'
     render :new
   end
-end
+  end
 
-def destroy
+  def destroy
   @task = Task.find(params[:id])
   @task.destroy
 
   flash[:success] = 'タスクが削除されました'
   redirect_to tasks_path
+  end
+
+  def create
+  @task=Task.new(task_params)
+  
+  if @task.save
+    flash[:success] = 'タスクが投稿されました'
+    redirect_to @task
+  else
+    flash[:danger] = 'タスクが投稿されませんでした'
+    render :new
+  end
+  end
+
+  def task_params
+  params.require(:task).permit(:content)
+  end
 end
