61e4f8e0c5dadd0b53ee8233815f4ce4a1080850
Ubuntu <ubuntu@ip-172-31-7-238.ap-northeast-1.compute.internal>
2021-03-30 05:42:38 +0000

    kadai commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,39 @@
+class TasksController < ApplicationController
+    
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+end
+
+def update
+  @task = Task.find(params[:id])
+
+  if @task.update(task_params)
+    flash[:success] = 'タスクが編集されました'
+    redirect_to @task
+  else
+    flash.now[:danger] = 'タスクが編集されませんでした'
+    render :new
+  end
+end
+
+def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+
+  flash[:success] = 'タスクが削除されました'
+  redirect_to tasks_path
+end
